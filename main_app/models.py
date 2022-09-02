0051ff9208ad7f92023183c4179d42065e00ce98
Jvilla125 <Jvilla125@gmail.com>
2022-09-02 16:19:33 -0700

    Made the Drinks model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
+
+INGREDIENT_CHOICES = (
+    ("1", "Tequila"),
+    ("2", "Vodka"),
+)
 
 # Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=500)
+    ingredients = forms.MultipleChoiceField(choices = INGREDIENT_CHOICES)
+    instructions = models.TextField(max_length=1000)
