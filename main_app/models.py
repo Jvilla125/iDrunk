2b8258c004a4c32c5b90680a4fb0d4c90bf4266a
jshprintz <jshprintz@gmail.com>
2022-09-06 11:44:03 -0700

    test

--- main_app/models.py
+++ main_app/models.py
@@ -2,22 +2,20 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User 
 from django.urls import reverse
-from multiselectfield import MultiSelectField
 
-
-INGREDIENT = (
-        ("1", "Tequila"),
-        ("2", "Vodka"),
-        ("3", "Rum"),
-        ("4", "Lemon"),
-        ("5", "Coke"),
-    )
+INGREDIENT_CHOICES = (
+    ("1", "Tequila"),
+    ("2", "Vodka"),
+    ("3", "Rum"),
+    ("4", "Lemon"),
+    ("5", "Coke"),
+)
 
 # Create your models here.
 class Drink(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
-    ingredients = MultiSelectField(choices = INGREDIENT, max_length=10)
+    ingredients = forms.MultipleChoiceField(choices = INGREDIENT_CHOICES, widget=forms.CheckboxSelectMultiple())
     instructions = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
