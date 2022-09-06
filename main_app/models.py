4955e6ecbbc28451e4ce52312b321cbd60764cf6
jshprintz <jshprintz@gmail.com>
2022-09-06 11:46:13 -0700

    multiselectfield

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User 
 from django.urls import reverse
+from multiselectfield import MultiSelectField
 
 INGREDIENT_CHOICES = (
     ("1", "Tequila"),
