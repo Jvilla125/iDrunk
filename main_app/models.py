b4b7fb69be1ef72a50cc6c0dae7ee079294a44af
jshprintz <jshprintz@gmail.com>
2022-09-07 10:46:30 -0700

    Ingredients list updated

--- main_app/models.py
+++ main_app/models.py
@@ -5,11 +5,18 @@ from django.urls import reverse
 from multiselectfield import MultiSelectField
 
 INGREDIENT = (
-    ("1", "Tequila"),
-    ("2", "Vodka"),
-    ("3", "Rum"),
-    ("4", "Lemon"),
-    ("5", "Coke"),
+    ("1", "Brandy"), ("2", "Gin"), ("3", "Rum"), ("4", "Tequila"),
+    ("5", "Vodka"), ("6", "Whiskey"), ("7", "Vodka"), ("8", "Absinthe"),
+    ("9", "Amaretto"), ("10", "Aperol"), ("11", "Baileys"), ("12", "Blue Curacao"),
+    ("13", "Campari"), ("14", "Chambord"), ("15", "Chartreause Green"),
+    ("16", "Chartreause Yellow"), ("17", "Cointreau"), ("18", "Creme de Cacao"),
+    ("19", "Creme de Cassis"), ("20", "Creme de Menthe"), ("21", "Frangelico"),
+    ("22", "Jagermeister"), ("23", "Kahlua"), ("24", "Midori"), ("25", "Triple Sec"),
+    ("26", "Club Soda"), ("27", "Cola"), ("28", "Cranberry Juice"), ("29", "Cream"),
+    ("30", "Diet Cola"), ("31", "Energy Drink"), ("32", "Ginger Ale"),
+    ("33", "Ginger Beer"), ("34", "Grapefruit Juice"), ("35", "Lemon Juice"),
+    ("36", "Lemon/Lime Soda"), ("37", "Lime Juice"), ("38", "Orange Juice"),
+    ("39", "Simple Syrup"), ("40", "Tomato Juice"), ("41", "Tonic")
 )
 
 # Create your models here.
